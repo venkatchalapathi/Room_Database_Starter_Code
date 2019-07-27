b42aefe5154734ac00b32d0704cf160aab89c355
Venkata Chalapathi <venkat.pv@gmail.com>
2019-07-27 09:56:36 +0530

    Initial commit

---
+++ app/src/main/java/com/venky/room_database_starter_code/MainActivity.java
@@ -0,0 +1,13 @@
+package com.venky.room_database_starter_code;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
